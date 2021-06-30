58d8ae76a4429fc20b9180c14e1aa275baf7d0c9
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-06-30 19:12:08 -0300

    Add files via upload
    Meu primeiro programa explicado com comentários.

---
+++ primeiro programa.py
@@ -0,0 +1,7 @@
+print('Hello World!')
+# os dados, se eles forem mensagens, eles tem delimitadores especiais que podem ser aspas simples ou duplas,
+# todos os comandos são funções, ou seja, precisam de ( )
+# para o Python, toda variável é um objeto
+# mensagens são usadas primordialmente para serem exibidas na tela
+# números são usados para fazer contas e, nesse caso, não necessitam de aspas
+# colocando os números em aspas, eles viram uma mensagem
