22200865b9f996e6ca3b1130147734762adb1d21
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-07-02 16:51:24 -0300

    Add files via upload

---
+++ média de notas.py
@@ -0,0 +1,5 @@
+p1 = float(input('Primeira nota de um aluno: '))
+p2 = float(input('Segunda nota de um aluno: '))
+m = (p1 + p1) / 2
+print('A média do aluno é de {}.'.format(m))
+# ou print(f'A média do aluno é de {(p1 + p2) / 2}.') dessa forma não precisaria da 3° linha
