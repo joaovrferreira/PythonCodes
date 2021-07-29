10e56d3eea061225de7c4057aefedb69c886067d
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-07-29 20:03:34 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,14 @@
+n = int(input('Digite o número que desejar: '))
+t1 = n * 1
+t2 = n * 2
+t3 = n * 3
+t4 = n * 4
+t5 = n * 5
+t6 = n * 6
+t7 = n * 7
+t8 = n * 8
+t9 = n * 9
+t10 = n * 10
+print('-=' * 10)
+print(' {}\n {}\n {}\n {}\n {}\n {}\n {}\n {}\n {}\n {}\n'.format(t1, t2, t3, t4, t5, t5, t7, t8, t9, t10))
+print('-=' * 10)
