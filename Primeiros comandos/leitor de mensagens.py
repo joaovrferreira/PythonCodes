1d3458d6eb9353a8f11919597e3e6a402059a430
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-08-03 16:11:43 -0300

    Create leitor de mensagens.py

---
+++ Primeiros comandos/leitor de mensagens.py
@@ -0,0 +1,6 @@
+nome = 'João'
+idade = 21
+peso = 65
+print(nome, idade, peso)
+# antes do sinal de igual, chamamos de variável (x/variável = print('Qual o seu nome?')
+# uma variável é um espaço dentro da memória do computador que podemos salvar coisas
