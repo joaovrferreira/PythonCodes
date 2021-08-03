d7d6b6442d4a9b3d951351026f9df5a12365fe82
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-08-03 16:11:49 -0300

    Create só um teste.py

---
+++ Primeiros comandos/só um teste.py
@@ -0,0 +1,4 @@
+print('Olá pessoinha!')
+question = input('Qual o seu nome terráqueo? ')
+# print('Seja bem vindo(a) a Marte,' + question + '!') tbm funciona
+print('Seja bem vindo(a) {}!'.format(question))
