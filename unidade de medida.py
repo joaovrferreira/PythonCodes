637c207757c6038a0f29f00a92c50c93ee9c414c
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-08-03 16:09:44 -0300

    Delete unidade de medida.py

--- unidade de medida.py
+++
@@ -1,4 +0,0 @@
-metros = int(input('Digite a quantidade em metros: '))
-centimetros = metros * 100
-milimetros = metros * 1000
-print('Seu antigo valor agora em cm {} e mm {}.'.format(centimetros, milimetros))
