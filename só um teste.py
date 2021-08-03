98f05c0defe617c006c3fd39b3a7a8d223051ef1
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-08-03 16:09:24 -0300

    Delete só um teste.py

--- só um teste.py
+++
@@ -1,4 +0,0 @@
-print('Olá pessoinha!')
-question = input('Qual o seu nome terráqueo? ')
-# print('Seja bem vindo(a) a Marte,' + question + '!') tbm funciona
-print('Seja bem vindo(a) {}!'.format(question))
