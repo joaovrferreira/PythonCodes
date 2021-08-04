ca6cae9f915e1873e75290c1b3d34c5dc3918458
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-08-04 13:42:55 -0300

    Create contas1.py

---
+++ Operadores aritméticos/contas1.py
@@ -0,0 +1,7 @@
+a = int(input('Digite um número qualquer: '))
+d = a * 2
+t = a * 3
+r = a ** (1/2)
+print('O dobro desse número é {}\n Seu triplo é {}\n E a raiz quadrada é {:.2f}.'.format(d, t, r))
+# ou r = a pow(1, 2)
+# ou print(f'O dobro desse número é {d}\n Seu triplo é {t}\n E a raiz quadrada é {r:.2f}.')
