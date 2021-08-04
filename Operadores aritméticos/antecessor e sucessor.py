30e1ede309e6835d187904ecb207f2c5a4543be8
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-08-04 13:42:39 -0300

    Create antecessor e sucessor.py

---
+++ Operadores aritméticos/antecessor e sucessor.py
@@ -0,0 +1,6 @@
+a = int(input('Digite um número qualquer: '))
+an = a - 1
+su = a + 1
+print('Seu antecessor é {} e seu sucessor é {}.'.format(an, su))
+# excluindo a linhas 2 e 3 :
+# ou poderia ser print('Seu antecessor é {} e seu sucesor é {}.'.format((a - 1), (a + 1))
