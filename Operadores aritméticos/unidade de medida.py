b166804e6f399377424bae961128b27fc147a6f7
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-08-09 12:19:35 -0300

    Create unidade de medida.py

---
+++ Operadores aritméticos/unidade de medida.py
@@ -0,0 +1,4 @@
+metros = int(input('Digite a quantidade em metros: '))
+centimetros = metros * 100
+milimetros = metros * 1000
+print('Seu antigo valor agora em cm {} e mm {}.'.format(centimetros, milimetros))
