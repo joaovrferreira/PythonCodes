cd0b343172d9f6b81c9b66ea1ddcff4ebedb7bda
João Vitor Reis Ferreira <81247955+joaovrferreira@users.noreply.github.com>
2021-08-10 20:40:42 -0300

    Create teoria de tipos primitivos.py

---
+++ Tipos Primitivos/teoria de tipos primitivos.py
@@ -0,0 +1,10 @@
+# a junção de 2 mensagens (strings) é chamada de concatenação e é feita pelo sinal de +
+# os tipos primitivos vem na frente do input
+# int é usado para números inteiros (ex: 7, -4, 0)
+# float é usado para números reais  ou com ponto flutuante, ou seja, números depois da vírgula (ex: 0.76, 7.0)
+# bool é usado para valores lógicos ou booleanos (ex: True ou False, os dois capitalizados)
+# str é usado para valores caracteres ou strings ('Olá', '5')
+# o tipo primitivo de uma variável tem que ser específicada no momento em que eu vou ler (print)
+s = 5
+print('A soma vale', s)
+print('A soma vale {}.'.format(s))
